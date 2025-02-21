203255d829995b000db59054a83a1a52e5557ddd
thebrandconsultant <james@thebrandconsultant.co.za>
2025-02-21 11:15:05 +0200

    Blog Example - Bootstrap

---
+++ Module 8/my-vue-blog-example/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app" class="container mt-5">
+    <h1 class="mb-4">My Blog</h1>
+    <div class="row">
+      <div class="col-md-4" v-for="(post, index) in posts" :key="index">
+        <BlogCard
+          :title="post.title"
+          :excerpt="post.excerpt"
+          :image="post.image"
+          :link="post.link"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BlogCard from './components/BlogCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BlogCard
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: 'First Post',
+          excerpt: 'This is a short summary of the first post.',
+          image: 'https://images.unsplash.com/photo-1735825764457-ffdf0b5aa5dd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          link: '#'
+        },
+        {
+          title: 'Second Post',
+          excerpt: 'This is a short summary of the second post.',
+          image: 'https://images.unsplash.com/photo-1735825764457-ffdf0b5aa5dd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          link: '#'
+        },
+        {
+          title: 'Third Post',
+          excerpt: 'This is a short summary of the third post.',
+          image: 'https://images.unsplash.com/photo-1735825764457-ffdf0b5aa5dd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          link: '#'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+/* You can add global styles here if needed */
+</style>
---
+++ Module 8/my-vue-blog-example/src/components/BlogCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card" style="width: 18rem;">
+      <img :src="image" class="card-img-top" alt="Blog image">
+      <div class="card-body">
+        <h5 class="card-title">{{ title }}</h5>
+        <p class="card-text">{{ excerpt }}</p>
+        <a :href="link" class="btn btn-primary">Read More</a>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BlogCard',
+    props: {
+      title: {
+        type: String,
+        default: 'Blog Title'
+      },
+      excerpt: {
+        type: String,
+        default: 'A short summary of the blog post.'
+      },
+      image: {
+        type: String,
+        default: 'https://via.placeholder.com/286x180'
+      },
+      link: {
+        type: String,
+        default: '#'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Add any custom styling here if needed */
+  </style>
+  
